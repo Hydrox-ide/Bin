08834c070b497b90365cfcb64710ff537232d3e1
Hydrox-ide <networkhyperion@gmail.com>
2015-03-13 10:52:47 -0700

    Fixed a lot of stuff, including permission messages, console commands, etc.

--- me/Hydroxide/Bin/Main.java
+++ me/Hydroxide/Bin/Main.java
@@ -1,6 +1,7 @@
 package me.Hydroxide.Bin;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -13,9 +14,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin implements Listener {
 	
 	public boolean onCommand (CommandSender sender, Command command, String commandLabel, String[] args) {
+		if(!(sender instanceof Player)) {
+			sender.sendMessage(ChatColor.RED + "/BIN is a player only command.");
+			return false;
+		}
 		Player player = (Player) sender;
 		if(commandLabel.equalsIgnoreCase("bin") && player.hasPermission("bin.bin")) {
 			openBin(player);
+			return true;
+		} else if (!(player.hasPermission("bin.bin"))) {
+			player.sendMessage(ChatColor.RED + "You do not have permission for that command.");
 		}
 		return false;
 	}
